c297f294b9030b7e650705f32abd2b271eb6c845
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-06-27 12:23:24 +0530

    Update HealthCheck.java

--- src/main/java/net/enginneringdigest/journalApp/Controller/HealthCheck.java
+++ src/main/java/net/enginneringdigest/journalApp/Controller/HealthCheck.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheck {
-    @GetMapping("/health-check")
+    @GetMapping("/health-check")                  
     public String HealthCheck(){
         return "Ok" ;
     }
